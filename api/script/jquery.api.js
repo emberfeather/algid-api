8a77f6c8692843447da09481fac8ee2e01a9a200
Randy Merrill <Zoramite@gmail.com>
2011-02-09 22:18:39 -0800

    Updating the api ajax call to use the Deferred object for added additional event handlers.
    Fixed the default for the api url to be static instead of based off a variable the may not be there.

--- api/script/jquery.api.js
+++ api/script/jquery.api.js
@@ -6,40 +6,30 @@
 		var original;
 		var opts = $.extend({}, $.api.defaults, options);
 		
-		// Save the original functions for later
-		original = {
-			success: opts.success
-		};
-		
-		// Override the success function
-		opts.success = function(data, textStatus, XMLHttpRequest) {
-			// Trigger the events for api alerts
-			triggerAlert((!data.HEAD.result ? [ data.HEAD.errors.HEAD.error.message, data.HEAD.errors.HEAD.error.detail ] : undefined), 'errors', opts.triggerElement);
-			triggerAlert(data.HEAD.errors, 'errors', opts.triggerElement);
-			triggerAlert(data.HEAD.warnings, 'warnings', opts.triggerElement);
-			triggerAlert(data.HEAD.successes, 'successes', opts.triggerElement);
-			triggerAlert(data.HEAD.messages, 'messages', opts.triggerElement);
-			
-			// Do the original success functionality
-			if(original.success){
-				original.success(data, textStatus, XMLHttpRequest);
-			}
-		};
-		
 		// Bring in the data
 		opts.data = {
 			HEAD: JSON.stringify(head),
 			BODY: JSON.stringify(body)
 		};
 		
-		$.ajax( opts );
+		var request = $.ajax( opts );
+		
+		// Add a callback to the ajax Deferred object
+		request.done(function(data, textStatus, XMLHttpRequest) {
+			// Trigger the events for api alerts
+			triggerAlert((!data.HEAD.result ? [ data.HEAD.errors.HEAD.error.message, data.HEAD.errors.HEAD.error.detail ] : undefined), 'errors', opts.triggerElement);
+			triggerAlert(data.HEAD.errors, 'errors', opts.triggerElement);
+			triggerAlert(data.HEAD.warnings, 'warnings', opts.triggerElement);
+			triggerAlert(data.HEAD.successes, 'successes', opts.triggerElement);
+			triggerAlert(data.HEAD.messages, 'messages', opts.triggerElement);
+		});
 	};
 	
 	$.api.defaults = {
 		dataType: 'json',
 		triggerElement: $('body'),
 		type: 'post',
-		url: $.algid.admin.options.base.url + $.algid.admin.options.base.api
+		url: '/api'
 	};
 	
 	function triggerAlert(alerts, type, element) {
